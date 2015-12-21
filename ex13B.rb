75dc07a8a993e10035c963601542e079d8f9fdc1
Sean Shields <sean@dmgctrl.com>
2015-12-21 17:46:35 -0500

    ex14 - Prompting and Passing

---
+++ ex13B.rb
@@ -0,0 +1,7 @@
+first, second, third, number = ARGV
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "your third variable is: #{third}"
+
+# to do - Combine gets.chomp with ARGV
